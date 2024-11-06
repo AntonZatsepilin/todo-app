448e4c5e9699bd75869c54d84385841e1b54b512
AntonZatsepilin <zatsepilin90.00@bk.ru>
2024-11-06 12:29:40 +0300

    add parse JWT token

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -8,6 +8,7 @@ import (
 type Authorization interface {
 	CreateUser(user models.User) (int, error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type TodoList interface {
